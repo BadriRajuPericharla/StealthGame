fbefb2d4bef8c36e16bbc9a7bab55281ac69f184
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-06-15 14:06:48 +0530

    Added Enemy Patrol

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+    private NavMeshAgent navMeshAgent;
+    bool goingToB=false;
+    void Start()
+    {
+        navMeshAgent=GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(PointA.position);
+    }
+    void Update()
+    {
+        if(!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.2f)
+        {
+            if (goingToB)
+            {
+                navMeshAgent.SetDestination(PointB.position);
+            }
+            else
+            {
+                navMeshAgent.SetDestination(PointA.position);
+            }
+            goingToB=!goingToB;
+        }
+    }
+}
