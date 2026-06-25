fa4ed9fbf89d7242717c629a7b0b19069634e2a2
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-06-25 12:34:15 +0530

    Updated Treasure Opening

--- Assets/Scripts/Keys/TreasureOpening.cs
+++ Assets/Scripts/Keys/TreasureOpening.cs
@@ -6,13 +6,14 @@ public class TreasureOpening : MonoBehaviour
 {
     public Keys keys;
     public GameObject player;
+    public float ClaimRange=3f;
     public InventoryManager inventoryManager;
     public string[] RequiredKeyNames;
     bool allKeysCollected;
     void Update()
     {
         float distance=Vector3.Distance(transform.position,player.transform.position);
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)&&distance<ClaimRange)
         {
             allKeysCollected=true;
             for(int i = 0; i < RequiredKeyNames.Length; i++)
