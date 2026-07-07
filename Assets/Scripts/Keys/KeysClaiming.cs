49659233fcf6b643449d746bef0e66c81552fdb0
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-07-07 11:00:43 +0530

    Changed The Keys Claiming Logic

--- Assets/Scripts/Keys/KeysClaiming.cs
+++ Assets/Scripts/Keys/KeysClaiming.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,38 +8,59 @@ public class KeysClaiming : MonoBehaviour
 {
     public InventoryManager inventory;
     public InventoryKeys inventoryKeys;
+    [SerializeField]private CinemachineVirtualCamera followCamera;
+    [SerializeField]private float claimDistance=3f;
     public Keys keys;
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (other.gameObject.tag == "DoorKey")
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            Debug.Log("Touched: " + other.name);
-            for(int i = 0; i < keys.DoorKeyPrefab.Length; i++)
-            {
-                if (other.gameObject.name.Contains(keys.DoorKeyPrefab[i].name))
-                {
-                    inventory.AddDoorKey(keys.DoorKeyName[i]);
-                    Color color=inventoryKeys.doorKeys[i].color;
-                    color.a=1;
-                    inventoryKeys.doorKeys[i].color=color;
-                    other.gameObject.SetActive(false);
-                    break;
-                }
-            }
+            KeyPickUp();
         }
-        if (other.gameObject.tag == "TreasureKey")
+    }
+    public void KeyPickUp()
+    {
+        int layerMask=~ LayerMask.GetMask("Player");
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(ray, out hit, claimDistance,layerMask))
         {
-            for(int i = 0; i < keys.TreasureKeyPrefab.Length; i++)
+            Debug.Log("Hit: " + hit.collider.name);
+
+            if (hit.collider.CompareTag("DoorKey"))
             {
-                if (other.gameObject.name.Contains(keys.TreasureKeyPrefab[i].name))
+                // Pick up the door key
+                Debug.Log("Touched: " + hit.collider.name);
+                for(int i = 0; i < keys.DoorKeyPrefab.Length; i++)
+                {
+                    if (hit.collider.gameObject.name.Contains(keys.DoorKeyPrefab[i].name))
+                    {
+                        inventory.AddDoorKey(keys.DoorKeyName[i]);
+                        Color color=inventoryKeys.doorKeys[i].color;
+                        color.a=1;
+                        inventoryKeys.doorKeys[i].color=color;
+                        hit.collider.gameObject.SetActive(false);
+                        break;
+                    }
+                }
+                }
+            else if (hit.collider.CompareTag("TreasureKey"))
+            {
+                for(int i = 0; i < keys.TreasureKeyPrefab.Length; i++)
+            {
+                if (hit.collider.gameObject.name.Contains(keys.TreasureKeyPrefab[i].name))
                 {
                     inventory.AddTreasureKey(keys.TreasureKeyName[i]);
                     Color color=inventoryKeys.treasureKeys[i].color;
                     color.a=1;
                     inventoryKeys.treasureKeys[i].color=color;
-                    other.gameObject.SetActive(false);
+                    hit.collider.gameObject.SetActive(false);
                     break;
                 }
+            }
             }
         }
     }
